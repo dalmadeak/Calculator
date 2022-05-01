a148a485922eb7375a90ea81774238c57c014d57
Dalma <cynder097@gmail.com>
2022-05-01 17:48:43 +0200

    Added basic functionalities

--- Calculator/src/app/calculator/calculator.component.ts
+++ Calculator/src/app/calculator/calculator.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-  input : String = '140';
-  result = new String;
+  input : string = '0';
+  result : string = '0';
+  isNewNumber : boolean = false;
 
   constructor() { }
 
@@ -15,7 +16,8 @@ export class CalculatorComponent implements OnInit {
   }
 
   clear() {
-
+    this.input = '0';
+    this.result = '0';
   }
 
   saveToMemory() {
@@ -27,15 +29,39 @@ export class CalculatorComponent implements OnInit {
   }
 
   pressNumber(number: any) {
+    if (this.isNewNumber) {
+      this.input = '0';
+    }
 
+    if (this.input != '0' ||
+        this.input == '0' && number == '.') {
+      this.input = this.input + number;
+    } else {
+      this.input = number;
+    }
+
+    this.isNewNumber = false;
   }
 
   pressOperator(operator: string) {
+    if (this.result != '0') {
+      this.getAnswer();
+    }
+
+    this.result = this.input + operator;
+    this.isNewNumber = true;
 
   }
 
   getAnswer() {
-
+    this.input = eval(this.result + this.input);
+    this.result = '0';
+    this.isNewNumber = true;
   }
 
 }
+
+/*
+Dupla operátorok
+Dupla pontok
+*/
